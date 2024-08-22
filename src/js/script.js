e232827160f50644cecd7cd9c32202feef6739c7
zotel1 <zotelsigel@gmail.com>
2024-08-22 17:07:11 -0300

    update js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,17 @@
-const nextYear = (new Date()).getFullYear + 1;
-
+const nextYear = (new Date()).getFullYear() + 1;
 const nextYearDate = new Date(nextYear, 0, 1);
 
 setInterval(() => {
-const todayDate = new Date();
+    const todayDate = new Date();
+    const secDiff = Math.floor((nextYearDate.getTime() - todayDate.getTime()) / 1000);
 
-const secDiff = Math.floor((nextYearDate.getTime() - todayDate.getTime() / 1000));
+    const days = Math.floor(secDiff / (60 * 60 * 24));
+    const hours = Math.floor((secDiff % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((secDiff % (60 * 60)) / 60);
+    const seconds = Math.floor(secDiff % 60);
 
-const days = Math.floor((secDiff / (60 * 60)) / 24);
-const hours = Math.floor((secDiff / (60 * 60)) % 24);
-const minutes = Math.floor(secDiff / 60) % 24;
-const seconds = Math.floor(secDiff) % 60;
-
-dayElement.innerText = days;
-hourElement.innerText = days;
-minElement.innerText = days;
-secElement.innerText = days;
+    document.getElementById('dayElement').innerText = days;
+    document.getElementById('hourElement').innerText = hours;
+    document.getElementById('minElement').innerText = minutes;
+    document.getElementById('secElement').innerText = seconds;
 }, 1000);
